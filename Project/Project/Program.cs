52bfda2f075e6d7096e5163909b0a39fe3a0664a
ZachariahPepper <peppzg1@student.op.ac.nz>
2020-05-12 12:03:05 +1200

    Fixed the problem where some items would repeat twice. Replaced switch statements with do while loops

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -12,7 +12,8 @@ namespace Project
 
         static void Main(string[] args)
         {
-            string name, bread, meat, salad, sauce, cheese, sides;
+            string name, bread, meat, salad = "", saladentered, sauce ="", sauceentered, cheese="", cheeseentered, sides = "", sidesentered;
+          
           
             
                         Console.WriteLine("Hello Welcome to SandBread");
@@ -44,7 +45,7 @@ namespace Project
             meat = Console.ReadLine();
             Console.WriteLine(meat + " for the meat");
             Console.ReadLine();
-                        
+                       
             Console.WriteLine("What Salads would you like? We have:");
             Console.WriteLine("Lettuce");
             Console.WriteLine("Spinach");
@@ -55,61 +56,18 @@ namespace Project
             Console.WriteLine("Olives");
             Console.WriteLine("Pickles");
             Console.WriteLine("Beetroot");
-            Console.WriteLine("Mushrooms");
+            Console.WriteLine("Mushrooms");         
             
-            switch (salad = Console.ReadLine())
+            do
             {
-                case "lettuce":
-                    salad = salad + "lettuce";
-                    break;
+                saladentered = Console.ReadLine();
+                
+                salad = salad + " " + saladentered;
 
-                case "spinach":
-                    salad = salad + "spinach";
-                    break;
 
-                case "tomato":
-                    salad = salad + "tomato";
-                    break;
+            } while (saladentered != "") ;
 
-                case "carrots":
-                    salad = salad + "arrots";
-                    break;
-
-                case "onion":
-                    salad = salad + "onions";
-                    break;
-
-                case "capsicum":
-                    salad = salad + "capsicum";
-                    break;
-
-                case "olives":
-                    salad = salad + "olives";
-                    break;
-
-                case "pickles":
-                    salad = salad + "pickles";
-                    break;
-
-                case "beetroot":
-                    salad = salad + "beetroot";
-                    break;
-
-                case "mushrooms":
-                    salad = salad + "mushrooms";
-                    break;
-
-                    default:
-                    salad = "No salad";
-                    break;
-                    
-
-            
-            
-            }
-            
-
-            Console.WriteLine("your salads are " + salad);
+            Console.WriteLine("your salads are" + salad);
             Console.ReadLine();
 
             Console.WriteLine("What sauces would you like? We have:");
@@ -120,40 +78,18 @@ namespace Project
             Console.WriteLine("Sweet Chilli");
             Console.WriteLine("Mayonaise");
 
-            switch (sauce = Console.ReadLine())
+            do
             {
-                case "barbecue":
-                    sauce = sauce + "barbecue";
-                    break;
+                sauceentered = Console.ReadLine();
 
-                case "aioli":
-                    sauce = sauce + "aioli";
-                    break;
-
-                case "thousand island":
-                    sauce = sauce + "thousand island";
-                    break;
-
-                case "ranch":
-                    sauce = sauce + "ranch";
-                    break;
-
-                case "sweet chilli":
-                    sauce = sauce + "sweet chilli";
-                    break;
-
-                case "mayonaise":
-                    sauce = sauce + "mayonaise";
-                    break;
-
-                default:
-                    sauce = "No sauce";
-                    break;
+                sauce = sauce + " " + sauceentered;
 
 
-            }
+            } while (sauceentered != "");
 
-            Console.WriteLine("Your sauces are " + sauce);
+            
+
+            Console.WriteLine("Your sauces are" + sauce);
             Console.ReadLine();
 
             Console.WriteLine("What chesse would you like? We have:");
@@ -162,88 +98,36 @@ namespace Project
             Console.WriteLine("Swiss");
             Console.WriteLine("American");
 
-            switch (cheese = Console.ReadLine())
+            do
             {
-                
+                cheeseentered = Console.ReadLine();
 
-                case "mozzarella":
-                    cheese = cheese + "mozzarella";
-                    break;
+                cheese = cheese + " " + cheeseentered;
 
-                case "cheddar":
-                    cheese = cheese + "cheddar";
-                    break;
 
-                case "swiss":
-                    cheese = cheese + "swiss";
-                    break;
+            } while (cheeseentered != "");
 
-                case "american":
-                    cheese = cheese + "american";
-                    break;
-
-                default:
-                    cheese = "No cheese";
-                    break;
-
-            }
-
-            Console.WriteLine("Your cheese's are " + cheese);
+            Console.WriteLine("Your cheese's are" + cheese);
             Console.ReadLine();
 
             Console.WriteLine("And finally would you like any sides? We have");
             Console.WriteLine("Drinks, The sizes are small, medium or large");
             Console.WriteLine("Cookies: Choc chip, M&M and Oat & Raisin");
             Console.WriteLine("Muffins: Choc chip, Boysenberry and Banana");
-            
-            switch (sides = Console.ReadLine())
+
+            do
             {
-                case "small drink":
-                    sides = sides + "small drink";
-                    break;
+                sidesentered = Console.ReadLine();
 
-                case "medium drink":
-                    sides = sides + "medium drink";
-                    break;
+                sides = sides + " " + sidesentered;
 
-                case "large drink":
-                    sides = sides + "large drink";
-                    break;
 
-                case "choc chip cookie":
-                    sides = sides + "choc chip cookies";
-                    break;
+            } while (sidesentered != "");
 
-                case "m&m cookie":
-                    sides = sides + "m&m cookie";
-                    break;
-
-                case "oat & raisin cookie":
-                    sides = sides + "oat & raisin cookie";
-                    break;
-
-                case "choc chip muffin":
-                    sides = sides + "choc chip muffin";
-                    break;
-
-                case "boysenberry muffin":
-                    sides = sides + "boysenberry muffin";
-                    break;
-
-                case "banana muffin":
-                    sides = sides + "banana muffin";
-                    break;
-
-                default:
-                    sides = "no sides";
-                    break;
-
-            }
-
-            Console.WriteLine("Your sides are " + sides);
+            Console.WriteLine("Your sides are" + sides);
             Console.ReadLine();
 
-            Console.WriteLine("Your Order is a " + bread + " bread with " + meat + " for the meat and " + salad + " for the salad, " + sauce + " sauce and " + cheese + " for the cheese and " + sides);
+            Console.WriteLine("Your Order is a " + bread + " bread with " + meat + " for the meat and" + salad + "for the salad," + sauce + "sauce and" + cheese + "for the cheese and" + sides + "for the sides");
             Console.ReadLine();
         }
     }
