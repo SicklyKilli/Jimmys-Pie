a0257ede88d2bdbdbd0f423060a79f74f0bbdeba
Yoseob Shim <shimy5@student.op.ac.nz>
2020-05-16 22:53:48 +1200

    Sides dictionary sideMenu added; Items added; Joseph S

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -11,8 +11,20 @@ namespace Project
 
         public static void Main()
         {
-            String[] menuItems = new string[] { "Bread", "Meat", "Salad", "Sauces??" };
-            Nav();
+            var sideMenu = new Dictionary<string, double>();
+            sideMenu.Add("Sweet Potato Fries", 4.00);
+            sideMenu.Add("Smoked Cheese Champ Mash", 4.00);
+            sideMenu.Add("Homemade Sumo Wedges", 4.00);
+            sideMenu.Add("Fries", 3.20);
+            sideMenu.Add("Spicy Fries", 3.20);
+            sideMenu.Add("Onion Rings", 2.50);
+            sideMenu.Add("Rice", 2.50);
+            sideMenu.Add("Regular Soup", 5.00);
+            sideMenu.Add("Vegan Soup", 6.00);
+            sideMenu.Add("Cheese Roll", 2.50);
+        
+
+
         }
 
         public static void Hello()
@@ -22,11 +34,14 @@ namespace Project
             Console.WriteLine("Whos the order for today?");
             name = Console.ReadLine();
             Console.WriteLine("Hello " + name);
+        }        
+
+
+        public static void Sides()
+        {
+
         }
 
-        public static void Nav()
-        {
-            Hello();
-        }
+
     }
 }
